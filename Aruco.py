6075823f84b32e16c3e9a3be3fc837ec578159ad
Anagh Kanungo <23740251+anaghkanungo7@users.noreply.github.com>
2022-01-27 11:02:38 -0500

    Version 2.0

--- Aruco.py
+++ Aruco.py
@@ -23,9 +23,49 @@ def findArucoMarkers(img, markerSize=6, totalMarkers=250, draw=True):
         gray, arucoDict, parameters=arucoParam)
 
     # print(ids)
-    # print(bbox)
+    # if (bboxes):
+    # x, y of first corner
+    # print(bboxes[0][0][0])
+
     if draw:
         aruco.drawDetectedMarkers(img, bboxes)
+
+    return [bboxes, ids]
+
+
+def augmentAruco(bboxes, id, img, imgAugment, drawId=True):
+
+    topLeft = bboxes[0][0][0], bboxes[0][0][1]
+    topRight = bboxes[0][1][0], bboxes[0][1][1]
+    bottomRight = bboxes[0][2][0], bboxes[0][2][1]
+    bottomLeft = bboxes[0][3][0], bboxes[0][3][1]
+
+    # Get height, width, channel of image to be augmented
+    height, width, channel = imgAugment.shape
+
+    # Now,
+    # we have to find the homography
+    # that will give us a matrix
+    # the matrix will help us to transform from one image to another
+    # this will give us the ability to overlay our image at the correct position
+    # on our destination image
+
+    # Image Transformation and Perspective Transformation - read up on this to understand it
+    pts1 = np.array([topLeft, topRight, bottomRight, bottomLeft])
+    pts2 = np.float32([[0, 0], [width, 0], [width, height], [0, height]])
+    matrix, _ = cv.findHomography(pts2, pts1)
+
+    # Warp Image
+    imgOut = cv.warpPerspective(
+        imgAugment, matrix, (img.shape[1], img.shape[0]))
+
+    # Fill original image where there are markers with black
+    cv.fillConvexPoly(img, pts1.astype(int), (0, 0, 0))
+
+    # Now, we can combine the images
+    imgOut = img + imgOut
+
+    return imgOut
 
 
 def main():
@@ -34,9 +74,23 @@ def main():
 
     while True:
         isTrue, frame = capture.read()
-        findArucoMarkers(frame)
-        cv.imshow('Image', frame)
+        # Part 1: Find aruco markers and return bounding boxes and IDs
+        arucoFound = findArucoMarkers(frame)
 
+        # Part 2: Augment something on the markers
+        imgAug = cv.imread('./Images/nyu.png')
+
+        # Loop through all markers and augment each one
+        # Check if bounding box list is not empty
+        if (len(arucoFound[0]) != 0):
+            # Loop through both Bboxes, ID at the same time
+            for bboxes, id in zip(arucoFound[0], arucoFound[1]):
+                # print(bboxes, id)
+
+                # Actual augment
+                frame = augmentAruco(bboxes, id, frame, imgAug)
+
+        cv.imshow('Image', frame)
         cv.waitKey(1)
 
 
